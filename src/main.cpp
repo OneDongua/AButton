921c57024e84580c266f7a998ce21e17c8143b2c
OneDongua <875658697@qq.com>
2025-01-29 20:39:21 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include <WiFi.h>
 #include <HTTPClient.h>
+#include <WiFi.h>
 
 // 定义 LED 与 按键引脚
 #define LED_PIN 8
@@ -19,42 +19,34 @@ bool status = false;
 void connectWiFi();
 void upload();
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLDOWN);
 
-  connectWiFi();
+  // connectWiFi();
 }
 
-void loop()
-{
+void loop() {
   // 按键消抖
-  if (digitalRead(BUTTON_PIN))
-  {
+  if (digitalRead(BUTTON_PIN)) {
     // 睡眠 10ms，如果依然为高电平，说明抖动已消失。
     delay(10);
-    if (digitalRead(BUTTON_PIN) && !status)
-    {
+    if (digitalRead(BUTTON_PIN) && !status) {
       led_logic = !led_logic;
       digitalWrite(LED_PIN, led_logic);
       upload();
       // led 的状态发生了变化，即使我持续按着按键，LED 的状态也不应该改变。
       status = !status;
-    }
-    else if (!digitalRead(BUTTON_PIN))
-    {
+    } else if (!digitalRead(BUTTON_PIN)) {
       status = false;
     }
   }
 }
 
-void upload()
-{
-  if (WiFi.status() == WL_CONNECTED)
-  {
+void upload() {
+  if (WiFi.status() == WL_CONNECTED) {
     // 创建 HTTPClient 对象
     HTTPClient http;
 
@@ -63,10 +55,10 @@ void upload()
     http.addHeader("Content-Type", "application/json");
     http.setTimeout(1000);
 
-    int httpCode = http.POST("{\"msg\": \"Hello from ESP32: " + String(led_logic ? "OFF" : "ON") + "\"}");
+    int httpCode = http.POST("{\"msg\": \"Hello from ESP32: " +
+                             String(led_logic ? "OFF" : "ON") + "\"}");
 
-    if (httpCode > 0)
-    {
+    if (httpCode > 0) {
       // 获取响应状态码
       Serial.printf("HTTP 状态码: %d", httpCode);
       Serial.print(" ");
@@ -75,30 +67,24 @@ void upload()
       String response = http.getString();
       Serial.print("响应数据：");
       Serial.println(response);
-    }
-    else
-    {
+    } else {
       Serial.print("错误：");
       Serial.println(http.errorToString(httpCode).c_str());
     }
     http.end();
-  }
-  else
-  {
+  } else {
     Serial.println("WiFi未连接");
   }
 }
 
-void connectWiFi()
-{
+void connectWiFi() {
   // 连接 WiFi
   WiFi.begin(ssid, password);
 
   Serial.print("正在连接 Wi-Fi");
 
   // 检测是否连接成功
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
